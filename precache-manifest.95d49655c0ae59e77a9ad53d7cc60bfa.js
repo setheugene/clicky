247e670a9460646d50feaaf26f77b6983cc6127a
setheugene <seth.eugene@icloud.com>
2019-04-14 13:16:09 -0500

    Updates

---
+++ precache-manifest.95d49655c0ae59e77a9ad53d7cc60bfa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4363465db862e89c37e",
+    "url": "/clicky/static/js/runtime~main.c6956f21.js"
+  },
+  {
+    "revision": "ac6cac5e94814ea14f14",
+    "url": "/clicky/static/js/main.e69afb89.chunk.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clicky/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "ac6cac5e94814ea14f14",
+    "url": "/clicky/static/css/main.9713c379.chunk.css"
+  },
+  {
+    "revision": "a88ea58e7d85b4b0c19298093fa72b64",
+    "url": "/clicky/index.html"
+  }
+];
